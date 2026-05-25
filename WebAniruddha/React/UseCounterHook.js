ff23eda8355f4b442ae2da8fff295e089ede4693
aniruddha-verma <anivp28@gmail.com>
2026-05-25 23:41:03 +0530

    Add useCounterHook that manages a counter state

---
+++ WebAniruddha/React/UseCounterHook.js
@@ -0,0 +1,25 @@
+import { useCallback, useState } from 'react';
+
+/**
+ * @param number initialValue
+ * @return Object
+ */
+export default function useCounter(initialValue = 0) {
+  const [count, setCount] = useState(initialValue);
+  const increment = useCallback(() => {
+    setCount((x) => x + 1);
+  }, []);
+  const decrement = useCallback(() => {
+    setCount((x) => x - 1);
+  }, []);
+  const reset = useCallback(() => {
+    setCount(initialValue);
+  }, [initialValue]);
+  return {
+    count,
+    increment,
+    decrement,
+    reset,
+    setCount,
+  };
+}
